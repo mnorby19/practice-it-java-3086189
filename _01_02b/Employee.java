d4e133830a4e4246bab22336c7d0976e0bb66699
mnorby19 <129117659+mnorby19@users.noreply.github.com>
2024-08-31 21:41:04 +0000

    y

--- _01_02b/Employee.java
+++ _01_02b/Employee.java
@@ -1,9 +1,9 @@
 package _01_02b;
 
+
 public class Employee {
 
   public static void main(String[] args) {
-
     // Create a variable called age of type int and assign it the value 29.
 
     // Print the age variable to the console.
@@ -11,29 +11,52 @@ public class Employee {
     // Create a variable called isAManager of type boolean and assign it the value
     // true.
 
-    // Print the isAManager variable to the console.
+    // Print the isAManager variable to the console. int age = 29;
+    int age = 29;
+    System.out.println("The person's age is: " + age);
+
+    Boolean isAManager = true;
+
+    if(isAManager == true){
+     System.out.println("The person is a manager.");
+     System.out.println("True");
+    }
+    else{
+      System.out.println("The person is not a manager.");
+      System.out.println("False");
+    }
 
     // Create a variable called yearsOfService of type double and assign it the
     // value 2.5.
 
     // Print the yearsOfService variable to the console.
+    Double yearsOfService = 2.5;
+    System.out.print("Years of service: " + yearsOfService);
+    System.out.print(yearsOfService);
+
 
     // Create a variable called baseSalary of type int and assign it the value 3000.
+    int baseSalary = 3000;
 
     // Create a variable called overtimePayment of type int and assign it the value
     // 40.
+    int overtimePayment = 40;
 
     // Create a variable called totalPayment of type int and assign it to the value
     // of baseSalary added to overtimePayment.
+    int totalPayment = baseSalary + overtimePayment;
 
     // Print the totalPayment variable to the console.
+    System.out.println(totalPayment);
 
     // Create three variables all of type double on a single line.
     // They should be called firstBonus, secondBonus and thirdBonus and they should
     // be assigned the values 10.00, 22.00 and 35.00.
+    double firstBonus = 10.00, secondBonus = 22.00, thirdBonus = 35.00;
 
     // Print out the sum of the variables called firstBonus, secondBonus and
     // thirdBonus.
+    System.out.println(firstBonus + secondBonus + thirdBonus);
 
   }
 
